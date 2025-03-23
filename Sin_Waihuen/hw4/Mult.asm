53238f1bb1a3deb4a894149444eda18759708efc
erb1 <erb1@ERBMGCW102.headquarters.erb.org>
2025-03-23 11:59:45 +0800

    Add homework 4

--- Sin_Waihuen/hw4/Mult.asm
+++ Sin_Waihuen/hw4/Mult.asm
@@ -1,30 +1,23 @@
- 	@i              
-        M=0            
-        @R2          
-        M=0           
-
+ @2
+    M=0     
+    @i
+    M=0     
 (LOOP)
-        @i
-        D=M             
+    @i
+    D=M     
+    @0
+    D=D-M  
+    @END
+    D;JGE    
 
-        @R0           
-        D=D-M        
-
-        @END
-        D;JEQ        
-
-        @R1      
-        D=M          
-
-        @R2
-        M=M+D      
-
-        @i
-        M=M+1     
-
-        @LOOP
-        0;JMP          
-
+    @1
+    D=M     
+    @2
+    M=D+M  
+    @i
+    M=M+1   
+    @LOOP
+    0;JMP   
 (END)
-        @END
-        0;JMP         
+    @END
+    0;JMP   
